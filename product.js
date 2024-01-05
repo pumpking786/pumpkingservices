a029739deceff4c2f288083732154405d8405fd8
Pramit Amatya <130182347+pumpking786@users.noreply.github.com>
2024-01-05 12:50:01 +0545

    Update product.js

--- product.js
+++ product.js
@@ -22,3 +22,24 @@ function messanger(){
 
                 }
         }
+const searching = () =>{
+            const searchbox= document.getElementById("search-author").value.toUpperCase();
+            const storeitems = document.getElementById("product-cards")
+            const product = document.querySelectorAll(".card")
+            const pname=storeitems.querySelectorAll(".author")
+
+            for(var i=0; i<pname.length; i++){
+                let match = product[i].querySelectorAll('.author')[0];
+
+                if(match){
+                   let textvalue= match.textContent || match.innerHTML
+
+                   if(textvalue.toUpperCase().indexOf(searchbox) > -1){
+                    product[i].style.display="";
+                   }else{
+                    product[i].style.display="none";
+                   }
+                }
+
+            }
+    }
