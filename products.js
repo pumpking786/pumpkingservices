6d92da85579f4853dd83af5e89727259d9b5d384
pumpking786 <pramitamatya786@gmail.com>
2024-03-23 14:27:16 +0545

    done

--- products.js
+++ products.js
@@ -43,3 +43,11 @@ function messanger(){
 
             }
     }
+    function showSidebar(){
+        const sidebar= document.querySelector('.sidebar')
+        sidebar.style.display='flex'
+    }
+    function hideSidebar(){
+        const sidebar= document.querySelector('.sidebar')
+        sidebar.style.display='none'
+    }
